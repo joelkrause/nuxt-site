f0605fdf07c38aac51d9e869c89ae599fffff0ad
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2020-03-08 21:36:12 +1100

    start of something cool

---
+++ components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+    <header>
+        <div class="logos">
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+            <nuxt-link to="/" class="logo">Joel Krause</nuxt-link>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header{
+    display: flex;
+    font-size: 3vw;
+    padding: 2rem;
+    white-space: nowrap;
+    max-width: 100vw;
+    overflow: hidden;
+    align-items: center;
+    height: 6vw;
+    position: relative;
+    .logos{
+        animation-duration: 100s;
+        animation-name: ticker;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+        position: absolute;
+    }
+    .logo{
+        text-transform: uppercase;
+        color:#fff;
+        text-decoration: none;
+        padding: 0 2rem 0 0;
+    }
+	@-webkit-keyframes ticker {
+	0% {
+		-webkit-transform: translate3d(0, 0, 0);
+		transform: translate3d(0, 0, 0);
+		visibility: visible;
+	}
+
+	100% {
+		-webkit-transform: translate3d(-100%, 0, 0);
+		transform: translate3d(-100%, 0, 0);
+	}
+	}
+
+	@keyframes ticker {
+	0% {
+		-webkit-transform: translate3d(0, 0, 0);
+		transform: translate3d(0, 0, 0);
+		visibility: visible;
+	}
+
+	100% {
+		-webkit-transform: translate3d(-100%, 0, 0);
+		transform: translate3d(-100%, 0, 0);
+	}
+	}
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <p>Hey, I'm Joel and i'm a frontend developer from Melbourne, Australia.</p>
+    <p>I work at raak creative building headless sites with wordpess and nuxt.js</p>
+  </main>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
