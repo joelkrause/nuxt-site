e2f8b4cef1ca8d67f0df239638f560c852506855
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2020-03-08 21:54:31 +1100

    home page update

---
+++ components/Social.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="social">
+<h2>Socials &mdash;</h2>
+<a href="#">Twitter</a>
+</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,27 @@
 <template>
   <main>
-    <p>Hey, I'm Joel and i'm a frontend developer from Melbourne, Australia.</p>
-    <p>I work at raak creative building headless sites with wordpess and nuxt.js</p>
+    <section class="intro">
+      <div class="intro_text">
+        <p>Hey, I'm Joel and i'm a frontend developer from Melbourne, Australia.</p>
+        <p>I work at raak creative building headless sites with wordpess and nuxt.js</p>
+      </div>
+    </section>
+    <Social />
   </main>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
+import Social from '~/components/Social.vue'
 
 export default {
   components: {
-    Header
+    Header,
+    Social
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.intro{}
+</style>
