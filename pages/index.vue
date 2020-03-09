a9c3ba3d8a2b2126639c06d2f29716174cb091f6
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2020-03-10 06:15:33 +1100

    updated layout

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <section class="intro">
       <div class="intro_text">
         <p>Hey, I'm Joel and i'm a frontend developer from Melbourne, Australia.</p>
-        <p>I work at raak creative building headless sites with wordpess and nuxt.js</p>
+        <p>I work at raak creative building beautiful headless sites with wordpess and nuxt.js</p>
       </div>
     </section>
     <Social />
