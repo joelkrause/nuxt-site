c11de67ad08102ea32e8af7d5c9a7e587beb67f5
Joel Krause <joelkrause@joels-mbp.home>
2020-03-14 22:25:36 +1100

    Updated home page ready to go live

--- components/Social.vue
+++ components/Social.vue
@@ -1,6 +1,15 @@
 <template>
-<div class="social">
-<h2>Socials &mdash;</h2>
-<a href="#">Twitter</a>
-</div>
+    <div class="social">
+    <h2>Socials &mdash;</h2>
+        <a href="https://twitter.com/Joel_Krause">Twitter</a>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.social{
+    a {
+        color:currentColor;
+        text-decoration: none;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,9 @@
     <section class="intro">
       <div class="intro_text">
         <p>Hey, I'm Joel and i'm a frontend developer from Melbourne, Australia.</p>
-        <p>I work at raak creative building beautiful headless sites with wordpess and nuxt.js</p>
+        <p>I work at <a href="https://raak.com.au" target="_blank">raak creative</a> building beautiful headless sites with nuxt.js</p>
       </div>
     </section>
-    <Social />
   </main>
 </template>
 
@@ -23,5 +22,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.intro{}
+main{
+    padding: 2rem;
+    @media (min-width:1280px){
+        max-width: 60vw;
+    }
+}
+.intro{
+  line-height: 1.5;
+  a{
+    color: currentColor;
+  }
+}
 </style>
